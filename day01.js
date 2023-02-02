37d27e1e737060adacb250bb4d55e1413b5fa288
sokritha-it <sokritha.it@gmail.com>
2023-02-02 16:32:41 +0700

    complete: day01

---
+++ day01.js
@@ -0,0 +1,13 @@
+console.log("This is my first day of training");
+console.log("1. My task is below");
+
+while (true) {
+  console.log("Study Git");
+}
+
+console.log("End of training");
+
+
+state of git:
+------------
+  working dir -> staging area -> local repo 
